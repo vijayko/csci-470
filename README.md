1668ecb3f135bedca52e34f3932410a102eb5197
vijayko <vijay.chaudhary@bison.howard.edu>
2023-09-05 10:25:18 -0400

    sharing the course material on github

---
+++ README.md
@@ -0,0 +1,5 @@
+### CSCI 470 Fundamentals of Algorithms Fall 2023 
+
+This repository will be temporarily up to share the lecture notes, homeworks, and lectures slides until I can post the course material on Canvas. 
+
+
---
+++ homeworks/homework-0/CSCI-470-Homework-Template-0/insertion_sort.py
@@ -0,0 +1,11 @@
+def insertion_sort(A):
+    '''
+    Input:  A | Python List of positive integers
+    Output: A | Python List of positive integers in non-decreasing order
+    '''
+    # You are not allowed to use built-in sort() method to sort the list, A. 
+    # You will receive no points if sort() method is used in the body of this function. 
+    ##################
+    # YOUR CODE HERE #
+    ##################
+    return A
